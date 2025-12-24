28e17cf5dfa4c221853d09016f90cc811ab9722f
sarvesh.devrukhkar <sarvesh.devrukhkar@fafadiatech.com>
2025-12-24 11:26:03 +0530

    Bring Post model data & passed into views function, so HTML template can access it.

--- DjangoGirlBlogApp/views.py
+++ DjangoGirlBlogApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 
 def post_listing(request):
-    return render(request, "DjangoGirlBlogApp/post_listing.html", {})
+    posts = Post.objects.filter(
+        published_date__lte=timezone.now().order_by("published_date")
+    )
+    return render(request, "DjangoGirlBlogApp/post_listing.html", {posts})
